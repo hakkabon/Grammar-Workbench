82e311e65bc33f49d85b5885f7d8abf2e5715b6c
Ulf Akerstedt-Inoue <hakkabon@gmail.com>
2026-08-05 22:34:49 +0900

    fixed errors

--- Tests/GrammarWorkbenchLSPTests/GrammarWorkbenchLSPServerTests.swift
+++ Tests/GrammarWorkbenchLSPTests/GrammarWorkbenchLSPServerTests.swift
@@ -2,7 +2,7 @@ import Foundation
 import XCTest
 import LanguageServerProtocol
 import LanguageServerProtocolTransport
-@testable import GrammarWorkbenchLSP
+@testable import grammar_workbench_lsp
 
 final class GrammarWorkbenchLSPServerTests: XCTestCase {
     private var client: TestClient!
